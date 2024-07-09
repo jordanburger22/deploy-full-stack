bfa8ac3edf65d565493527a46d77fefa5996f144
Jordan Burger <jordanburger22@gmail.com>
2024-07-09 19:18:18 +0000

    added server code

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ require('dotenv').config()
 const { expressjwt } = require('express-jwt')
 const movieRouter = require('./routes/movieRouter')
 const authRouter = require('./routes/authRouter')
+const path = require('path')
 
 
 app.use(express.json())
 app.use(morgan('dev'))
-
+// Serve static files from the 'client/dist' directory
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
 
 const connectToDB = async() => {
@@ -27,5 +29,9 @@ connectToDB()
 app.use('/api/auth', authRouter)
 app.use('/api/main', expressjwt({ secret: process.env.SECRET, algorithms: ['HS256'] }))
 app.use('/api/main/movie', movieRouter)
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.listen(process.env.PORT, () => console.log(`Server is running on PORT: ${process.env.PORT}`))
